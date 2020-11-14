b9f7c9507d8fe18e45d84c15fa4819b1f6061a4e
Tan-YiFan <15711657500@163.com>
2020-11-14 20:26:47 +0800

    add display

--- src/Rust01_HelloWorld/display.rs
+++ src/Rust01_HelloWorld/display.rs
@@ -3,11 +3,66 @@ use std::fmt;
 struct Structure(i32);
 
 
-mod display {
+mod display {}
 
-}
 impl fmt::Display for Structure {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
+        write!(f, "{}", self.0)
     }
+}
+
+#[derive(Debug)]
+struct MinMax(i32, i32);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}, {}", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+fn main() {
+    let minmax = MinMax(0, 14);
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let bigrange = MinMax(-300, 300);
+    let smallrange = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small range is {small}",
+             small = smallrange,
+             big = bigrange);
+    let point = Point2D { x: 3.3, y: 7.2 };
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let complex = Complex { real: 3.3, imag: 7.2 };
+    println!("Compare complexes:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
 }
