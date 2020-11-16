7a395ada8e8c9bcb36ad727e2ae32fe52831c23a
Tan-YiFan <15711657500@163.com>
2020-11-16 08:36:27 +0800

    update enums

--- src/Rust03_CustomTypes/enums.rs
+++ src/Rust03_CustomTypes/enums.rs
@@ -17,8 +17,11 @@ enum WebEvent {
 // returns nothing.
 fn inspect(event: WebEvent) {
     // switch case in C
+
+    // 'use'
+    use WebEvent::{PageLoad, PageUnload, Paste, KeyPress, Click};
     match event {
-        WebEvent::PageLoad => println!("page loaded"),
+        /*WebEvent::*/PageLoad => println!("page loaded"),
         WebEvent::PageUnload => println!("page unloaded"),
         // Destructure `c` from inside the `enum`.
         WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
@@ -26,23 +29,37 @@ fn inspect(event: WebEvent) {
         // Destructure `Click` into `x` and `y`.
         WebEvent::Click { x, y } => {
             println!("clicked at x={}, y={}.", x, y);
-        },
+        }
     }
+}
+
+// C-like enum
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
 }
 
 fn main() {
     let pressed = WebEvent::KeyPress('x');
     // `to_owned()` creates an owned `String` from a string slice.
-    let pasted  = WebEvent::Paste("my text".to_owned());
-    let click   = WebEvent::Click { x: 20, y: 80 };
-    let load    = WebEvent::PageLoad;
-    let unload  = WebEvent::PageUnload;
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
 
     inspect(pressed);
     inspect(pasted);
     inspect(click);
     inspect(load);
     inspect(unload);
+
+    // `enums` can be cast as integers.
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
 }
 
 // type alias
