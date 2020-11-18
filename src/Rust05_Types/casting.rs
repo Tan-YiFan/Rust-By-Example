0f24bcf0fffc32ee9e8a10fb8ada0a2eba5e4cb6
Tan-YiFan <15711657500@163.com>
2020-11-18 09:47:47 +0800

    add casting

---
+++ src/Rust05_Types/casting.rs
@@ -0,0 +1,40 @@
+// no implicit casting
+// explicit casting via `as`
+
+#![allow(overflowing_literals)]
+
+fn main() {
+    let decimal = 65.4321f32;
+
+    // Error
+    // let integer: u8 = decimal;
+
+    // explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    // There are limitations. Floats cannot be converted to char
+    // let character = decimal as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    // when casting any value to an unsigned type, T,
+    // T::MAX + 1 is added or subtracted until the value
+    // fits into the new type
+
+    println!("1000 as a u16 is: {}", 1000 as u16);
+    println!("1000 as a u8 is: {}", 1000 as u8);
+    println!("  -1 as a u8 is: {}", (-1i8) as u8);
+
+    // For positive numbers, this is the same as the modulus
+    println!("1000 mod 256 is : {}", 1000 % 256);
+
+    // When casting to a signed type, the (bitwise) result is the same as
+    // first casting to the corresponding unsigned type. If the most significant
+    // bit of that value is 1, then the value is negative.
+
+    // Unless it already fits, of course.
+    println!(" 128 as a i16 is: {}", 128 as i16);
+    // 128 as u8 -> 128, whose two's complement in eight bits is:
+    println!(" 128 as a i8 is : {}", 128 as i8);
+}
