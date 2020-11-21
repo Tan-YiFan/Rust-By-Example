5b4b5946f32959e7395c1eb1f51f9272540bd415
Tan-YiFan <15711657500@163.com>
2020-11-21 09:58:16 +0800

    add for loop

---
+++ src/Rust08_ControlFlow/forloop.rs
@@ -0,0 +1,63 @@
+fn main() {
+
+    // for in
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // for and iterate
+
+    // iter() leaving the collection untouched
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustocean"),
+            _ => println!("Hello, {}", name),
+        }
+    }
+
+    // into_iter() consumes the collection. No reuse. Exact data is provided.
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustocean"),
+            _ => println!("Hello, {}", name),
+        }
+    }
+    /* this is wrong
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustocean"),
+            _ => println!("Hello, {}", name),
+        }
+    }
+    */
+
+    // iter_mut allows modifying
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustocean among us!",
+            _ => "Hello",
+        }
+    }
+    println!("names: {:?}", names);
+}
