ddbd3b64191a25ff200a92ea4f2b286a9de17eae
Tan-YiFan <15711657500@163.com>
2020-11-23 22:10:50 +0800

    add visibility notes

---
+++ src/Rust10_Modules/visibility.rs
@@ -0,0 +1,8 @@
+// pub
+// pub(in path) only visible within the given path. `path` must be a parent or ancestor module
+// pub(self) is the same as private
+// pub(super) only visible within the parent module
+
+fn main() {
+
+}
